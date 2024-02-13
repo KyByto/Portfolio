8b4c3369427dac820e6bf28e8d02f848a4b67f61
Kybyto <136782115+KyByto@users.noreply.github.com>
2024-02-13 21:15:35 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
+      <header>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      </header>
       <body className={inter.className}>{children}</body>
     </html>
   );
