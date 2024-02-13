696ce4b6abb559835f6687d9c7acb6b4defebda6
Kybyto <136782115+KyByto@users.noreply.github.com>
2024-02-13 21:05:55 +0100

    change icon react position

--- app/models/Work.ts
+++ app/models/Work.ts
@@ -1,7 +1,7 @@
 
 const works :Work[] =[{
     "id": 1 ,
-"image" : "/WorkImage/zara.PNG",
+"image" : "https://i.ibb.co/1TBwfWx/zara.png",
 "title" : "Kyto",
 "link":"https://kyoto-clothes.netlify.app/",
 "description" : "This is  home page website for clothing that I developed using React.js and Swiper.js  and Sass",
@@ -10,7 +10,7 @@ const works :Work[] =[{
 
 },{
     "id": 1 ,
-"image" : "/WorkImage/dash.png",
+"image" : "https://i.ibb.co/zrVcqy8/dash.png",
 "title" : "Dash Event",
 "link" :"https://dash-event.netlify.app/",
 "description" :"My Website for the Google Developer Group Algiers challenge. Users can easily register for the Dash event and receive automatic mail."
@@ -19,7 +19,7 @@ const works :Work[] =[{
 
 },{
     "id": 1 ,
-"image" : "/WorkImage/portfolio.PNG",
+"image" : "https://i.ibb.co/d5QrqKH/portfolio.png",
 "title" : "Portfolio",
 "description" : " My portfolio is built with Next.js and TypeScript, ensuring top performance and clean, error-free code.",
 "technologies" : ["Next js" , "Typescript" , "Tailwind" ],
