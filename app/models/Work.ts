d7aaebf255edee5c151e23af3b73e0336add71a9
Kybyto <136782115+KyByto@users.noreply.github.com>
2024-07-24 15:02:07 +0100

    Update Work.ts

--- app/models/Work.ts
+++ app/models/Work.ts
@@ -1,73 +1,67 @@
-
-const works :Work[] =[{
-    "id": 0 ,
-"image" : "https://i.ibb.co/7pdJR48/Capture-d-cran-2024-06-04-065654.png",
-"title" : "Blog Website",
-"link":"https://github.com/KyByto/Blog-Website",
-"description" : "Blog Website Where Users Can register, log in, create, read, update, and delete their own blog posts, and view and interact with posts from other users.",
-"technologies" : ["Laravel" ,  "Tailwind css" , "Docker" ],
-"githublink" : "https://github.com/KyByto/Blog"
-"link" :"https://github.com/KyByto/Blog"
-},
-                      {
-    "id": 1 ,
-"image" : "https://i.ibb.co/1TBwfWx/zara.png",
-"title" : "Kyto",
-"link":"https://kyoto-clothes.netlify.app/",
-"description" : "This is  home page website for clothing that I developed using React.js and Swiper.js  and Sass",
-"technologies" : ["React js" , "Swiper js" , "Sass" ],
-"githublink" : "https://github.com/KyByto/Kyto-Clothes-Website-Home"
-
-},
-                      {
-    "id": 2 ,
-"image" : "https://i.postimg.cc/Y0QT7qcP/Capture-d-cran-2024-04-27-175916.png",
-"title" : "Ecom for Phone & Accessory Shop ",
-"link":"https://dev-vtcdz.pantheonsite.io/",
-"description" : "E-commerce website for A phone Shop ",
-"technologies" : ["Woo commerce" , "WordPress"  ],
-"githublink" : ""
-
-},
-                      {
-    "id": 3,
-"image" : "https://i.ibb.co/Nm8rPD0/Capture-d-cran-2024-06-04-072313.png",
-"title" : "E-Commerce Website",
-"link":"https://dev-eattallah-modeliste.pantheonsite.io/",
-"description" : "E-Commerce Website For Eatallah Modeliste",
-"technologies": ["WordPress" ] ,
-                          "githublink":""
-
-},
-                      {
-    "id": 4 ,
-"image" : "https://i.ibb.co/zrVcqy8/dash.png",
-"title" : "Dash Event",
-"link" :"https://dash-pi.vercel.app/",
-"description" :"My Website for the Google Developer Group Algiers challenge. Users can easily register for the Dash event and receive automatic mail."
-,"technologies" : ["Next js" , "Tailwind" , "TypeScript"  ],
-"githublink" : "https://github.com/KyByto/dash"
-
-},{
-    "id": 5 ,
-"image" : "https://i.ibb.co/d5QrqKH/portfolio.png",
-"title" : "Portfolio",
-"description" : " My portfolio is built with Next.js and TypeScript, ensuring top performance and clean, error-free code.",
-"technologies" : ["Next js" , "Typescript" , "Tailwind" ],
-"githublink" : "https://github.com/KyByto/Portfolio",
-    "link":"https://www.kybyto.tech",
-
-}
-          ,{
-    "id": 6 ,
-"image" : "https://i.ibb.co/rcQzp3W/hanoi.png",
-"title" : "Hanoi-tower",
-"description" : " Experience the classic Tower of Hanoi puzzle game online, challenging your strategic thinking with multiple difficulty levels.",
-"technologies" : ["Html" , "Css" , "JavaScript" ],
-"githublink" : "https://github.com/KyByto/Hanoi-tower",
-              "link":"https://hanoitower-complexity.netlify.app/"
-
-}
+const works: Work[] = [
+    {
+        "id": 0,
+        "image": "https://i.ibb.co/7pdJR48/Capture-d-cran-2024-06-04-065654.png",
+        "title": "Blog Website",
+        "link": "https://github.com/KyByto/Blog-Website",
+        "description": "Blog Website Where Users Can register, log in, create, read, update, and delete their own blog posts, and view and interact with posts from other users.",
+        "technologies": ["Laravel", "Tailwind css", "Docker"],
+        "githublink": "https://github.com/KyByto/Blog"
+    },
+    {
+        "id": 1,
+        "image": "https://i.ibb.co/1TBwfWx/zara.png",
+        "title": "Kyto",
+        "link": "https://kyoto-clothes.netlify.app/",
+        "description": "This is home page website for clothing that I developed using React.js and Swiper.js and Sass",
+        "technologies": ["React js", "Swiper js", "Sass"],
+        "githublink": "https://github.com/KyByto/Kyto-Clothes-Website-Home"
+    },
+    {
+        "id": 2,
+        "image": "https://i.postimg.cc/Y0QT7qcP/Capture-d-cran-2024-04-27-175916.png",
+        "title": "Ecom for Phone & Accessory Shop",
+        "link": "https://dev-vtcdz.pantheonsite.io/",
+        "description": "E-commerce website for A phone Shop",
+        "technologies": ["Woo commerce", "WordPress"],
+        "githublink": ""
+    },
+    {
+        "id": 3,
+        "image": "https://i.ibb.co/Nm8rPD0/Capture-d-cran-2024-06-04-072313.png",
+        "title": "E-Commerce Website",
+        "link": "https://dev-eattallah-modeliste.pantheonsite.io/",
+        "description": "E-Commerce Website For Eatallah Modeliste",
+        "technologies": ["WordPress"],
+        "githublink": ""
+    },
+    {
+        "id": 4,
+        "image": "https://i.ibb.co/zrVcqy8/dash.png",
+        "title": "Dash Event",
+        "link": "https://dash-pi.vercel.app/",
+        "description": "My Website for the Google Developer Group Algiers challenge. Users can easily register for the Dash event and receive automatic mail.",
+        "technologies": ["Next js", "Tailwind", "TypeScript"],
+        "githublink": "https://github.com/KyByto/dash"
+    },
+    {
+        "id": 5,
+        "image": "https://i.ibb.co/d5QrqKH/portfolio.png",
+        "title": "Portfolio",
+        "link": "https://www.kybyto.tech",
+        "description": "My portfolio is built with Next.js and TypeScript, ensuring top performance and clean, error-free code.",
+        "technologies": ["Next js", "Typescript", "Tailwind"],
+        "githublink": "https://github.com/KyByto/Portfolio"
+    },
+    {
+        "id": 6,
+        "image": "https://i.ibb.co/rcQzp3W/hanoi.png",
+        "title": "Hanoi-tower",
+        "link": "https://hanoitower-complexity.netlify.app/",
+        "description": "Experience the classic Tower of Hanoi puzzle game online, challenging your strategic thinking with multiple difficulty levels.",
+        "technologies": ["Html", "Css", "JavaScript"],
+        "githublink": "https://github.com/KyByto/Hanoi-tower"
+    }
 ];
 
-export  default works;
+export default works;
